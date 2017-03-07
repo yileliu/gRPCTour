c41218d89bbc94c889e90eb8d061273f288f9457
yileliu <neal_liu@outlook.com>
2017-03-07 08:40:09 +0800

    add logic to server

--- govacationtracker/src/server/main.go
+++ govacationtracker/src/server/main.go
@@ -1,43 +1,45 @@
 package main
 
 import (
-    "log"
-    "net"
-    "errors"
-    "fmt"
-    "io"
-    pb "../pb"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net"
+
+	pb "../pb"
 	context "golang.org/x/net/context"
 	grpc "google.golang.org/grpc"
-    "google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/metadata"
 )
 
 const port = ":6000"
+
 var nextId int32 = 100
 
 type employeeService struct{}
 
-func(s *employeeService) GetByBadgeNumber(ctx context.Context, req *pb.GetByBadgeNumberRequest) (*pb.EmployeeResponse, error){
-    if md, ok := metadata.FromContext(ctx); ok{
-        fmt.Println("Metadata received: %v\n", md)
-    }
+func (s *employeeService) GetByBadgeNumber(ctx context.Context, req *pb.GetByBadgeNumberRequest) (*pb.EmployeeResponse, error) {
+	if md, ok := metadata.FromContext(ctx); ok {
+		fmt.Println("Metadata received: %v\n", md)
+	}
 
-    for _, e := range employees{
-        if(req.BadgeNumber == e.BadgeNumber){
-            fmt.Println("Employee found: %v\n", e)
-            return &pb.EmployeeResponse{Employee: &e}, nil
-        }
-    }
+	for _, e := range employees {
+		if req.BadgeNumber == e.BadgeNumber {
+			fmt.Println("Employee found: %v\n", e)
+			return &pb.EmployeeResponse{Employee: &e}, nil
+		}
+	}
 
-    return nil, errors.New("Employee not found")
+	return nil, errors.New("Employee not found")
 }
 
-func(s *employeeService) GetAll(req *pb.GetAllRequest, stream pb.EmployeeService_GetAllServer) (error){
-    for _, e := range employees{
-        stream.Send(&pb.EmployeeResponse{Employee: &e})
-    }
+func (s *employeeService) GetAll(req *pb.GetAllRequest, stream pb.EmployeeService_GetAllServer) error {
+	for _, e := range employees {
+		stream.Send(&pb.EmployeeResponse{Employee: &e})
+	}
 
-    return nil
+	return nil
 }
 
 // func(s *employeeService) AddPhoto(stream pb.EmployeeService_AddPhotoServer) (error){
@@ -62,52 +64,65 @@ func(s *employeeService) GetAll(req *pb.GetAllRequest, stream pb.EmployeeService
 //     return nil
 // }
 
-func GetNextEmployeeId() (nextId int32){
-     nextId++
-     return
+func GetNextEmployeeId() {
+	nextId = nextId + 1
+	fmt.Println(nextId)
 }
 
-func (s *employeeService) Save(ctx context.Context, req *pb.EmployeeRequest) (*pb.EmployeeResponse, error){
-    employee := req.Employee
-    if(employee.Id == 0){
-        employee.Id = GetNextEmployeeId()
-    }
+func (s *employeeService) Save(ctx context.Context, req *pb.EmployeeRequest) (*pb.EmployeeResponse, error) {
+	employee := req.Employee
 
-    employees = append(employees, *employee)
+	employees = append(employees, *employee)
 
-    return &pb.EmployeeResponse{Employee:employee}, nil
+	return &pb.EmployeeResponse{Employee: employee}, nil
 }
-func (s *employeeService) SaveAll(stream pb.EmployeeService_SaveAllServer) error{
-    for{
-        emp, err := stream.Recv()
-        if err == io.EOF {
-            break;
-        }
+func (s *employeeService) SaveAll(stream pb.EmployeeService_SaveAllServer) error {
+	for {
+		var err error
+		var emp *pb.EmployeeRequest
+		emp, err = stream.Recv()
 
-        if err != nil {
-             return err
-        }
+		if emp.Employee.Id == 0 {
+			GetNextEmployeeId()
+			emp.Employee.Id = nextId
+		}
 
-        employees = append(employees, *emp.Employee)
-        stream.Send(&pb.EmployeeResponse{Employee: emp.Employee})
-    }
+		if err == io.EOF {
+			break
+		}
 
-    for _, e := range employees{
-        fmt.Println(e)
-    }
-    return nil
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(emp.Employee)
+		//employees = append(employees, *emp.Employee)
+		err = stream.Send(&pb.EmployeeResponse{Employee: emp.Employee})
+
+		if err != nil {
+			return err
+		}
+	}
+
+	fmt.Println("Here")
+
+	fmt.Println(len(employees))
+	for _, e := range employees {
+		fmt.Println(e)
+	}
+	return nil
 }
 
-func main(){
-    lis, err := net.Listen("tcp", port)
+func main() {
+	lis, err := net.Listen("tcp", port)
 
-    if err != nil{
-        log.Fatal(err)
-    }
-  
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	s := grpc.NewServer()
 
 	pb.RegisterEmployeeServiceServer(s, &employeeService{})
-    log.Println("Starting server on port" + port)
+	log.Println("Starting server on port" + port)
 	s.Serve(lis)
 }
